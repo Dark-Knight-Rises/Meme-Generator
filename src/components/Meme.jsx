49b4c97ede30819e322152b86a7f2b8a6a4237a0
Piyush <piyushat25@gmail.com>
2022-01-24 03:52:41 +0530

    working

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,17 +1,48 @@
 import React from 'react'
 
 function Meme() {
+    const arr = [
+        '🎮',
+        '🎯',
+        '🕹️',
+        '🎳',
+        '🏓',
+        '🏐',
+        '🎱',
+        '🏏',
+        '🥌',
+        '👾',
+        '👽'
+    ]
+    const [emoji, setEmoji] = React.useState(arr[0])
     const [meme, setMeme] = React.useState({
         topText: '',
         bottomText: '',
         randomImage: 'https://i.imgflip.com/61o4op.jpg'
     });
+    const [allMemes, setAllMemes] = React.useState([])
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
     const handleChange = (event) => {
         const { name, value } = event.target
         setMeme(prevMeme => ({
             ...prevMeme,
             [name]: value
         }))
+    }
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const randNo2 = Math.floor(Math.random() * arr.length)
+        const url = allMemes[randomNumber].url
+        const newEmoji = arr[randNo2]
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        setEmoji(newEmoji)
     }
     return (
         <main>
@@ -27,7 +58,7 @@ function Meme() {
                     onChange={handleChange}
                 />
             </form>
-            <button className='form--btn'><h3>Generate 🎮</h3></button>
+            <button className='form--btn' onClick={getMemeImage}><h3>Generate Random {emoji}</h3></button>
             <div className="meme">
                 <h3 className='meme--txt'>{meme.topText}</h3>
                 <img src={meme.randomImage} alt="image" className='meme--img' />
