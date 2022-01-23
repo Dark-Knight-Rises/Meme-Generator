d1f75e50108e672b0ab6d236da2883cdb7fd7ca0
Piyush <piyushat25@gmail.com>
2022-01-23 21:36:31 +0530

    third

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
 
 function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: '',
+        bottomText: '',
+        randomImage: 'https://i.imgflip.com/61o4op.jpg'
+    });
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
     return (
         <main>
             <form className='container'>
-                <input type="text" className='child' placeholder='Top Text' />
-                <input type="text" className='child' placeholder='Bottom Text' />
+                <input type="text" className='child' placeholder='Top Text'
+                    name='topText'
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
+                <input type="text" className='child' placeholder='Bottom Text'
+                    name='bottomText'
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
                 <button className='form--btn'><h3>Generate 🎮</h3></button>
             </form>
-            <img src="https://i.imgflip.com/61o4op.jpg" alt="image" className='meme--img' />
+
+            <img src={meme.randomImage} alt="image" className='meme--img' />
         </main>
     )
 }
